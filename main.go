171622340bbde337e2fd91b84b142ccc245b4fec
andykimchris <andrew.muchiri97@gmail.com>
2023-09-07 12:49:40 +0300

    chore: sets chi router and server to listen

--- main.go
+++ main.go
@@ -3,21 +3,36 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file :", err)
+	envErr := godotenv.Load()
+	if envErr != nil {
+		log.Fatal("Error loading .env file :", envErr)
 	}
 
 	portStr := os.Getenv("PORT")
 	if portStr == "" {
 		log.Fatal("PORT is not set")
 	}
+	fmt.Println("Server is running on port:", portStr)
 
-	fmt.Println("App is running on port: ", portStr)
+	router := chi.NewRouter()
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portStr,
+	}
+
+	serverErr := server.ListenAndServe()
+	if serverErr != nil {
+		log.Fatal(serverErr)
+	}
+
 }
